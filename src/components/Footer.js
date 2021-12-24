8830b2d858bc9566afaa881933980a6317d0e29c
vikhil0609 <sallagargivikhil123@gmail.com>
2021-12-24 18:13:22 +0530

    lot of changes made

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 // import React from "react";
-import "../assests/styles/Home/contact.css"
+import "../assests/styles/footer.css"
 import Logo from "../assests/images/Logo.png"
 import React from "react";
 import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
@@ -7,6 +7,7 @@ import { Facebook } from 'react-bootstrap-icons';
 
 const FooterPage = () => {
   return (
+    <div>
     <MDBFooter id="footer-main" className="font-small pt-4 mt-4">
       <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
@@ -51,7 +52,7 @@ const FooterPage = () => {
           </MDBCol>
           <MDBCol id="footer-links" style={{"position":"relative"}} md="3">
             <h5 className="title text-right">
-            <Facebook color="#3BB0A0" className="ml-4" />
+            <Facebook color="#3BB0A0" className=" text-xl  ml-4" />
             <Facebook color="#3BB0A0" className="ml-4"  />
             <Facebook color="#3BB0A0" className="ml-4" />
             <Facebook color="#3BB0A0"  className="ml-4" />
@@ -64,60 +65,11 @@ const FooterPage = () => {
               Copyright &copy;  {new Date().getFullYear()} Skylark Hatcheries Pvt. Ltd 
               </MDBContainer>
             </div>
+            <div className="footer-last">
+            </div>
     </MDBFooter>
+    </div>
   );
 }
 
 export default FooterPage;
-
-// class Footer extends React.Component{
-//     constructor(props){
-//         super(props); 
-//         this.state = {
-//             color:props.color
-//         }
-//         console.log(this.state.color)
-//       }
-//     render(){
-//     return(
-//         <div className="contact-home">
-//             <div className="contact-us" style={{backgroundColor:this.state.color}}>
-//             <div  id="about_us">
-//                 <img src={Logo} />
-//                 <div>
-//                     Skylark Feed is wholesome and nutritional
-//                     feed, most preffered meal in the Indian
-//                     polutyr market today. The various types of
-//                     feed manufactured by us are well-Blanaced
-//                     to fulfill the different requirements
-//                     of different birds
-//                 </div>
-//             </div>
-//                 <div id="links">
-//                     <h3>UseFul Links</h3>
-//                     <div className="sub">Lorem ipsum</div>
-//                     <div className="sub">Lorem ipsum</div>
-//                     <div className="sub">Lorem ipsum</div>
-//                     <div className="sub">Lorem ipsum</div>                    
-//                 </div>
-//                 <div id="contact">
-//                     <h3>Contact US</h3>
-//                     <div className="sub">Lorem ipsum</div>
-//                     <div className="sub">Lorem ipsum</div>
-//                     <div className="sub">Lorem ipsum</div>
-//                     <div className="sub">Lorem ipsum</div>
-//                 </div>
-//                 <div>
-//                 <div className="footer">
-//                 <i style={{fontSize:"24px"}} className="fa">&#xf16a;</i>
-//                 <i style={{fontSize:"24px"}} className="fa">&#xf09a;</i>
-//                 <i style={{fontSize:"24px"}} className="fa">&#xf0e1;</i>
-//                 </div>
-//                 </div>
-//         </div>
-// </div>
-
-//     );
-// }
-// }
-// export default Footer;
