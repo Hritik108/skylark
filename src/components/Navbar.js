47b97d47aea279ab6eeafb551d6375d5000a431a
Mahipal108 <mahipalsundvesha@gmail.com>
2021-12-24 16:10:14 +0530

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import jQuery from "jquery";
 import Egg from "../assests/images/egg.png"
 
 class Navbar extends React.Component{
+
     constructor(props){
         super(props); 
         this.state = {
@@ -27,52 +28,71 @@ class Navbar extends React.Component{
       }
 
       handleScroll(event) {
-        let scrollTop = event.srcElement.body.scrollTop,
-        itemTranslate = Math.min(0, scrollTop/3 - 60);
+    //     let scrollTop = event.srcElement.body.scrollTop,
+    //     itemTranslate = Math.min(0, scrollTop/3 - 60);
   
   
-        if(event.srcElement.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
-            jQuery(".menuA").css({"background-color":"rgba(255,255,255)",position:"fixed"})
-            this.setState({color:"green"});
-          }
-             else{  
-               jQuery(".menuA").css({"background-color":"transparent",position:"sticky"})
-               this.setState({color:"#75B8AD"});
-    }
-}
-    render(){
-    return(
-    <div id="Scroll" className= {`header-wrap${this.state.isPositionFixed ? ' fixed' : ""}`} style={{backgroundColor:"transparent"}} onScroll = {this.handleScroll} >
-    <div className="header">
-        <div className="menuA">
-            <div className="nav-left">
-            <NavLink id="navid" to="/" activeClassName="circle" style={{color:this.state.color}}>
-                HOME
-            </NavLink>
-            <NavLink id="navid" to="/products" style={{color:this.state.color}}>
-                PRODUCTS
-            </NavLink>
-            <NavLink id="navid" to="/gallery" style={{color:this.state.color}}>
-                GALLERY
-            </NavLink>
-        </div>
-        <div className="headimg">
-            <img src={Logo} /></div>
-        <div className="nav-right">
-            <NavLink id="navid" to="/quality" style={{color:this.state.color}} >
-                QUALITY
-            </NavLink >
-            <NavLink id="navid" to="career"   style={{color:this.state.color}}>
-                CAREER
-            </NavLink>
+    //     if(event.srcElement.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+    //         jQuery(".menuA").css({"background-color":"rgba(255,255,255)",position:"fixed"})
+    //         this.setState({color:"green"});
+    //       }
+    //          else{  
+    //            jQuery(".menuA").css({"background-color":"transparent",position:"sticky"})
+    //            this.setState({color:"#75B8AD"});
+    // }
+    window.onscroll = function() {myFunction()};
 
-            <NavLink id="navid" to="/contact" style={{color:this.state.color}}>
-                CONTACT
-            </NavLink>
-        </div>
-        </div>
-    </div>
-    </div>
+        var navbar = document.getElementById("navbar");
+        var sticky = navbar.offsetTop;
+
+        function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky");
+        }
+        }
+}
+
+    render(){
+        
+    return(
+        <div id="navbar">
+            <a class="active" href="javascript:void(0)">Home</a>
+            <a href="javascript:void(0)">News</a>
+            <a href="javascript:void(0)">Contact</a>
+            </div>
+    // <div id="Scroll" className= {`header-wrap${this.state.isPositionFixed ? ' fixed' : ""}`} style={{backgroundColor:"transparent"}} onScroll = {this.handleScroll} >
+    // <div className="header">
+    //     <div className="menuA">
+    //         <div className="nav-left">
+    //         <NavLink id="navid" to="/" activeClassName="circle" style={{color:this.state.color}}>
+    //             HOME
+    //         </NavLink>
+    //         <NavLink id="navid" to="/products" style={{color:this.state.color}}>
+    //             PRODUCTS
+    //         </NavLink>
+    //         <NavLink id="navid" to="/gallery" style={{color:this.state.color}}>
+    //             GALLERY
+    //         </NavLink>
+    //     </div>
+    //     <div className="headimg">
+    //         <img src={Logo} /></div>
+    //     <div className="nav-right">
+    //         <NavLink id="navid" to="/quality" style={{color:this.state.color}} >
+    //             QUALITY
+    //         </NavLink >
+    //         <NavLink id="navid" to="career"   style={{color:this.state.color}}>
+    //             CAREER
+    //         </NavLink>
+
+    //         <NavLink id="navid" to="/contact" style={{color:this.state.color}}>
+    //             CONTACT
+    //         </NavLink>
+    //     </div>
+    //     </div>
+    // </div>
+    // </div>
     )
 }
 }
