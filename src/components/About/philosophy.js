8ef296edc9900cfc41b669b0106419691dd9adfd
vikhil0609 <sallagargivikhil123@gmail.com>
2021-12-24 15:43:22 +0530

    changes made

--- src/components/About/philosophy.js
+++ src/components/About/philosophy.js
@@ -4,9 +4,16 @@ import jQuery from "jquery";
 
 class Philosophy extends Component{
     render(){
-        // function changes(){
-        //     jQuery("#small-circle1").css({"width":"340px","height":"340px","transition":"2s"})
-        // }
+        function changing_to_new(){
+            const w = jQuery("#big-circle").width()
+            const h = jQuery("#big-circle").height() - 200
+            console.log(h)
+            jQuery("#changing").animate({"height":h,"width":w},500,"linear")
+        }
+
+        function changing_to_old(){
+            jQuery("#changing").animate({"height":"320px","width":"320px"},500,"linear")
+        }
         return(
             <div className="quality-philosophy">
                     <div>
@@ -14,7 +21,7 @@ class Philosophy extends Component{
                         <table className="quality-names">
                             <tr>
                                 <td rowSpan="2" id="big-circle"><div>Quality <br/>Processes</div></td>
-                                <td  id="small-circle1"><div>Quality <br/> Livestock</div></td>
+                                <td  id="small-circle1" onMouseEnter={() => changing_to_new()} onMouseLeave={() => changing_to_old()} ><div id="changing">Quality <br/> Livestock</div></td>
                             </tr>
                             <tr>
                             <td id="small-circle2"><div>Quality <br/> Livestock</div></td>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,55 +1,118 @@
-import React from "react";
+// import React from "react";
 import "../assests/styles/Home/contact.css"
 import Logo from "../assests/images/Logo.png"
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
-class Footer extends React.Component{
-    constructor(props){
-        super(props); 
-        this.state = {
-            color:props.color
-        }
-        console.log(this.state.color)
-      }
-    render(){
-    return(
-        <div className="contact-home">
-            <div className="contact-us" style={{backgroundColor:this.state.color}}>
-            <div  id="about_us">
-                <img src={Logo} />
-                <div>
-                    Skylark Feed is wholesome and nutritional
-                    feed, most preffered meal in the Indian
-                    polutyr market today. The various types of
-                    feed manufactured by us are well-Blanaced
-                    to fulfill the different requirements
-                    of different birds
-                </div>
-            </div>
-                <div id="links">
-                    <h3>UseFul Links</h3>
-                    <div className="sub">Lorem ipsum</div>
-                    <div className="sub">Lorem ipsum</div>
-                    <div className="sub">Lorem ipsum</div>
-                    <div className="sub">Lorem ipsum</div>                    
-                </div>
-                <div id="contact">
-                    <h3>Contact US</h3>
-                    <div className="sub">Lorem ipsum</div>
-                    <div className="sub">Lorem ipsum</div>
-                    <div className="sub">Lorem ipsum</div>
-                    <div className="sub">Lorem ipsum</div>
-                </div>
-                <div>
-                <div className="footer">
-                <i style={{fontSize:"24px"}} className="fa">&#xf16a;</i>
-                <i style={{fontSize:"24px"}} className="fa">&#xf09a;</i>
-                <i style={{fontSize:"24px"}} className="fa">&#xf0e1;</i>
-                </div>
-                </div>
-        </div>
-</div>
+const FooterPage = () => {
+  return (
+    <MDBFooter id="footer-main" className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="3">
+          <img src={Logo} alt="footer_logo" className="footer-Image media-lefy" />
+            <p id="footer-text" className="text-xl-left">
+              Skylark feed is wholesum and nutritional feed, moset preffered
+              meal in the Indian polutry market today. The various types of
+              feed manufactured by us are well-balanced to fulfil the 
+              different requirements of different birds.
+            </p>
+          </MDBCol>
+          <MDBCol id="footer-links" md="3">
+            <h5 className="title">Useful Links</h5>
+              <li className="list-unstyled">
+                Lorem Ipsum
+              </li>
+              <li className="list-unstyled">
+                Lorem Ipsum
+              </li>
+              <li className="list-unstyled">
+                Lorem Ipsum
+              </li>
+              <li className="list-unstyled">
+                Lorem Ipsum
+              </li>
+          </MDBCol> 
+          <MDBCol id="footer-links" md="3">
+            <h5 className="title">Contacts Us</h5>
+              <li className="list-unstyled">
+                Lorem Ipsum
+              </li>
+              <li className="list-unstyled">
+                Lorem Ipsum
+              </li>
+              <li className="list-unstyled">
+                Lorem Ipsum
+              </li>
+              <li className="list-unstyled">
+                Lorem Ipsum
+              </li>
+          </MDBCol>
+          <MDBCol id="footer-links" md="3">
+            <h5 className="title">Links</h5>
+            <div className="footer-copyright align-text-bottom">
+        <MDBContainer fluid>
+          Copyright &copy;  {new Date().getFullYear()} Skylark Hatcheries Pvt. Ltd 
+        </MDBContainer>
+      </div>
+          </MDBCol>   
+        </MDBRow>
+      </MDBContainer>
 
-    );
+    </MDBFooter>
+  );
 }
-}
-export default Footer;
+
+export default FooterPage;
+
+// class Footer extends React.Component{
+//     constructor(props){
+//         super(props); 
+//         this.state = {
+//             color:props.color
+//         }
+//         console.log(this.state.color)
+//       }
+//     render(){
+//     return(
+//         <div className="contact-home">
+//             <div className="contact-us" style={{backgroundColor:this.state.color}}>
+//             <div  id="about_us">
+//                 <img src={Logo} />
+//                 <div>
+//                     Skylark Feed is wholesome and nutritional
+//                     feed, most preffered meal in the Indian
+//                     polutyr market today. The various types of
+//                     feed manufactured by us are well-Blanaced
+//                     to fulfill the different requirements
+//                     of different birds
+//                 </div>
+//             </div>
+//                 <div id="links">
+//                     <h3>UseFul Links</h3>
+//                     <div className="sub">Lorem ipsum</div>
+//                     <div className="sub">Lorem ipsum</div>
+//                     <div className="sub">Lorem ipsum</div>
+//                     <div className="sub">Lorem ipsum</div>                    
+//                 </div>
+//                 <div id="contact">
+//                     <h3>Contact US</h3>
+//                     <div className="sub">Lorem ipsum</div>
+//                     <div className="sub">Lorem ipsum</div>
+//                     <div className="sub">Lorem ipsum</div>
+//                     <div className="sub">Lorem ipsum</div>
+//                 </div>
+//                 <div>
+//                 <div className="footer">
+//                 <i style={{fontSize:"24px"}} className="fa">&#xf16a;</i>
+//                 <i style={{fontSize:"24px"}} className="fa">&#xf09a;</i>
+//                 <i style={{fontSize:"24px"}} className="fa">&#xf0e1;</i>
+//                 </div>
+//                 </div>
+//         </div>
+// </div>
+
+//     );
+// }
+// }
+// export default Footer;
