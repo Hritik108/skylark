0901d74e14745b13c3c944dd9252c802b59d5223
vikhil0609 <sallagargivikhil123@gmail.com>
2022-01-09 18:15:10 +0530

    animation in the circles

--- src/components/About/philosophy.js
+++ src/components/About/philosophy.js
@@ -4,8 +4,30 @@ import jQuery from "jquery";
 
 class Philosophy extends Component{
     render(){
+       function change_to_new(sel){
+            var mainw = jQuery("#big-circle").width()
+            var mainh = jQuery("#big-circle").height() - 200
+
+            var side1w = jQuery("#small-circle1").width()
+            var side1h = jQuery("#small-circle1").height()
+
+            var side2w = jQuery("#small-circle2").width()
+            var side2h = jQuery("#small-circle2").height()
+            if (sel === "small_1"){
+                jQuery("#small_1_changing").animate({"height":mainh,"width":mainw},500,"linear")
+                jQuery("#big_changing").animate({"height":side2h,"width":side2w},500,"linear")
+            }
+
+            if (sel === "small_2"){
+                jQuery("#small_2_changing").animate({"height":mainh,"width":mainw},500,"linear")
+                jQuery("#big_changing").animate({"height":side1h,"width":side1w},500,"linear")
+            }
+        }
+
+
+       function change_to_old(sel){
         var mainw = jQuery("#big-circle").width()
-        var mainh = jQuery("#big-circle").height() - 200
+        var mainh = jQuery("#big-circle").height()
 
         var side1w = jQuery("#small-circle1").width()
         var side1h = jQuery("#small-circle1").height()
@@ -13,25 +35,18 @@ class Philosophy extends Component{
         var side2w = jQuery("#small-circle2").width()
         var side2h = jQuery("#small-circle2").height()
 
-        function changing_to_new1(){
-            console.log(side1w,side1h)
-            jQuery("#small_1_changing").animate({"height":mainh,"width":mainw},500,"linear")
-            jQuery("#big_changing").animate({"height":side1h,"width":side1w},500,"linear")
-        }
-        function changing_to_new2(){
-            console.log(side2w,side2h)
-            jQuery("#small_2_changing").animate({"height":mainh,"width":mainw},500,"linear")
-            jQuery("#big_changing").animate({"height":side2h,"width":side2w},500,"linear")
+        if(sel === "small_1"){
+        jQuery("#big_changing").animate({"height":side1h,"width":side1w},500,"linear")
+        jQuery("#small_1_changing").animate({"height":side2h,"width":side2w -150},500,"linear")
         }
 
-        function changing_to_old1(){
-            jQuery("#small_1_changing").animate({"height":side1h,"width":side1w},500,"linear")
-            jQuery("#big_changing").animate({"height":mainw,"width":mainw},500,"linear")
+        if (sel === "small_2"){
+            jQuery("#big_changing").animate({"height":side2h,"width":side2w},500,"linear")
+            jQuery("#small_2_changing").animate({"height":side1h,"width":side1w -150},500,"linear")
         }
-        function changing_to_old2(){
-            jQuery("#big_changing").animate({"height":mainw,"width":mainw},500,"linear")
-            jQuery("#small_2_changing").animate({"height":side2h,"width":side2w},500,"linear")
-        }
+
+
+       }
         return(
             <div className="quality-philosophy">
                     <div>
@@ -40,10 +55,10 @@ class Philosophy extends Component{
                             <tbody>
                             <tr>
                                 <td rowSpan="2" id="big-circle"><div id="big_changing" >Quality <br/>Processes</div></td>
-                                <td  id="small-circle1" onMouseEnter={() => changing_to_new1()} onMouseLeave={() => changing_to_old1()} ><div id="small_1_changing">Quality <br/> Livestock</div></td>
+                                <td  id="small-circle1" onMouseEnter={() => change_to_new("small_1")} onMouseLeave={() => change_to_old("small_1")} ><div id="small_1_changing">Quality <br/> Livestock</div></td>
                             </tr>
                             <tr>
-                            <td id="small-circle2" onMouseEnter={() => changing_to_new2()} onMouseLeave={() => changing_to_old2()}><div id="small_2_changing">Quality <br/> Livestock</div></td>
+                            <td id="small-circle2" onMouseEnter={() => change_to_new("small_2")} onMouseLeave={() => change_to_old("small_2")}><div id="small_2_changing">Quality <br/> Livestock</div></td>
                             </tr>
                             </tbody>
                         </table>
