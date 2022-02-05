a89a206b6460f96afd3225638b3497ff95e0bc67
unknown <chauhanritik786@gmail.com>
2022-02-05 13:50:55 +0530

    product3

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,10 +1,12 @@
 import Product_cart from "../components/product/product_cart";
 import React from "react";
+import FooterPage from "../components/Footer";
 class productPage extends React.Component {
     render(){
         return(
         <div>
-           <Product_cart /> 
+           <Product_cart />
+           <FooterPage/> 
         </div>
         );
     }
