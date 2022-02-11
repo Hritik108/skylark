08ac6510f723c2f18aebcf26ca58816bb850999d
Shashi Prajapati <shashiprajj@gmail.com>
2022-02-11 12:33:07 +0530

    Commit changes done in Products

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,7 +1,6 @@
 import Product_cart from "../components/product/product_cart";
 import EnquiryPage from "../components/product/enquiry";
 import Carousell from "../components/product/carousel";
-import ProductNavbar from "../components/product/carousel_nav";
 import NavBar from "../components/Navbar"
 import React from "react";
 import FooterPage from "../components/Footer";
@@ -15,7 +14,6 @@ class productPage extends React.Component {
                     <NavBar />
                     <Carousell />
                 </div>
-                {/* <Carousell /> */}
                 <Product_cart />
                 <EnquiryPage />
                 <FooterPage />
