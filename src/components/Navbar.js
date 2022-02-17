83648e320e3a03b64e252a667541f4ed7715de4b
vikhil0609 <sallagargivikhil123@gmail.com>
2022-02-17 23:40:03 +0530

    changes made

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,103 +1,103 @@
 import React from "react";
-import {NavLink,Router} from "react-router-dom";
+import { NavLink, Router } from "react-router-dom";
 import "../assests/styles/navbar.css"
 import Logo from "../assests/images/Logo.png"
 import jQuery from "jquery";
 import Egg from "../assests/images/egg.png"
 
-class Navbar extends React.Component{
+class Navbar extends React.Component {
 
-    constructor(props){
-        super(props); 
+    constructor(props) {
+        super(props);
         this.state = {
-            active :"home" ,
+            active: "home",
             isPositionFixed: false,
-            isImage : true,
-            color:"",
+            isImage: true,
+            color: "",
 
         }
         this.handleScroll = this.handleScroll.bind(this);
-      }
+    }
 
-      componentDidMount() {
+    componentDidMount() {
         window.addEventListener('scroll', this.handleScroll);
-      }
+    }
 
-      componentWillUnmount() {
+    componentWillUnmount() {
         window.removeEventListener('scroll', this.handleScroll);
-      }
+    }
 
-      handleScroll(event) {
-    //     let scrollTop = event.srcElement.body.scrollTop,
-    //     itemTranslate = Math.min(0, scrollTop/3 - 60);
-  
-  
-    //     if(event.srcElement.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
-    //         jQuery(".menuA").css({"background-color":"rgba(255,255,255)",position:"fixed"})
-    //         this.setState({color:"green"});
-    //       }
-    //          else{  
-    //            jQuery(".menuA").css({"background-color":"transparent",position:"sticky"})
-    //            this.setState({color:"#75B8AD"});
-    // }
-    // window.onscroll = function() {myFunction()};
+    handleScroll(event) {
 
-    //     var navbar = document.getElementById("navbar");
-    //     var sticky = navbar.offsetTop;
-
-    //     function myFunction() {
-    //     if (window.pageYOffset >= sticky) {
-    //         navbar.classList.add("sticky")
-    //     } else {
-    //         navbar.classList.remove("sticky");
-    //     }
-    //     }
-}
-
-    render(){
-        
-    return(
+        let scrollTop = event.srcElement.body.scrollTop,
+            itemTranslate = Math.min(0, scrollTop / 3 - 60);
 
 
-        <nav className="navbar navbar-inverse navbar-fixed-top sticky navbar-expand-lg">
-    <a className="navbar-brand d-lg-none" href="#"><img className="Nav-logo" src={Logo}  /></a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbarToggler7"
-        aria-controls="myNavbarToggler7" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="myNavbarToggler7">
-        <ul className="navbar-nav left-side" >
-            <li className="nav-item">
-                <a className="nav-link text-left" href="/">HOME</a>
-            </li>
-            <li className="nav-item nav-left">
-                <a className="nav-link" href="/products">PRODUCTS</a>
-            </li>
+        if (event.srcElement.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+            jQuery(".navbar").css({ "background-color": "white", position: "sticky",height:"80px"})
+        }
+        else {
+            jQuery(".navbar").css({ "background-color": "transparent",position:"sticky",height:"100px"})
 
-            <li className="nav-item nav-left">
-                <a className="nav-link" href="/gallery">GALLERY</a>
-            </li>
-        </ul>
-        <ul className="navbar-nav mx-auto">
-            <a className="d-none d-lg-block" href="#"><img className="Nav-logo" src={Logo} /></a>
-        </ul>
-        <ul className="navbar-nav right-side">    
-            <li className="nav-item">
-                <a className="nav-link" href="/quality">QUALITY</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="/career">CAREER</a>
-            </li>
+        }
+        // window.onscroll = function() {myFunction()};
 
-            <li className="nav-item nav-left">
-                <a className="nav-link" href="/contact">CONTACT</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+        //     var navbar = document.getElementById("navbar");
+        //     var sticky = navbar.offsetTop;
+
+        //     function myFunction() {
+        //     if (window.pageYOffset >= sticky) {
+        //         navbar.classList.add("sticky")
+        //     } else {
+        //         navbar.classList.remove("sticky");
+        //     }
+        //     }
+    }
+
+    render() {
+
+        return (
+
+
+            <nav className="navbar navbar-inverse navbar-fixed-top sticky navbar-expand-lg">
+                <a className="navbar-brand d-lg-none" href="#"><img className="Nav-logo" src={Logo} /></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbarToggler7"
+                    aria-controls="myNavbarToggler7" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="myNavbarToggler7">
+                    <ul className="navbar-nav left-side" >
+                        <li className="nav-item">
+                            <a className="nav-link text-left" href="/">HOME</a>
+                        </li>
+                        <li className="nav-item nav-left">
+                            <a className="nav-link" href="/products">PRODUCTS</a>
+                        </li>
+
+                        <li className="nav-item nav-left">
+                            <a className="nav-link" href="/gallery">GALLERY</a>
+                        </li>
+                    </ul>
+                    <ul className="navbar-nav mx-auto">
+                        <a className="d-none d-lg-block" href="#"><img className="Nav-logo" src={Logo} /></a>
+                    </ul>
+                    <ul className="navbar-nav right-side">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/quality">QUALITY</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/career">CAREER</a>
+                        </li>
+
+                        <li className="nav-item nav-left">
+                            <a className="nav-link" href="/contact">CONTACT</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         )
 
-}
+    }
 }
 export default Navbar;
 
